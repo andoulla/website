1a56a96a0d915aa4e7ee3beef437d5979d82b183
Mariandi <mariandi.stylianou@atomlearning.co.uk>
2026-07-22 17:53:57 +0300

    Update src/views/resume/Resume.constants.ts

--- src/views/resume/Resume.constants.ts
+++ src/views/resume/Resume.constants.ts
@@ -1,4 +1,3 @@
-// Resume-owned URL params. Kept here (not in skillsUrlParams) to stay parallel-safe.
 export const SKILL_PARAM = 'skill';
 export const RECOMMENDATION_PARAM = 'recommendation';
 export const FOCUS_PARAM = 'focus';
